725da2f0ec7255ec2db3c379b1ccda114a6c2396
Adam Rankin <adamr14@vt.edu>
2019-12-04 19:01:38 -0500

    Element Boosters for damage and elements drawn on battle scene

--- battle.js
+++ battle.js
@@ -87,6 +87,7 @@ fightScene.prototype.drawEnemyHalf = function(x, y){
         noStroke();
         fill(95, 35, 35);
         text(this.enemyP.type, 55, 75, 100, 100);
+        drawElement(this.enemyP.elementType, 0.3, 45, 40);
         text("LVL "+this.enemyP.level, 150, 80, 100, 100);
         rect(55, 95, 120, 12, 5);
         fill(255, 255, 255);
@@ -128,6 +129,7 @@ fightScene.prototype.drawPlayerHalf = function(x, y){
     fill(95, 35, 35);
     textSize(11);
     text(this.currPokemon.type, 240, 45, 100, 100);
+    drawElement(this.currPokemon.elementType, 0.3, 230, 10);
     text("LVL "+this.currPokemon.level, 340, 50, 100, 100);
     rect(240, 65, 120, 12, 5);
     fill(255, 255, 255);
@@ -274,7 +276,10 @@ fightScene.prototype.execute = function(){
                     pokeballMenu=false;
                 }
             }
-
+            text(this.enemyP.checkElementTypes(this.currPokemon.elementType), 200, 200);
+            text(this.currPokemon.checkElementTypes(this.enemyP.elementType), 200, 220);
+            text(this.enemyP.elementType, 220, 200);
+            text(this.currPokemon.elementType, 220, 220);
             break;
         case 4: //loss
             this.drawEnemyHalf(0,0);
@@ -433,7 +438,8 @@ fightScene.prototype.pAttack = function(){
         if(this.currPokemon.object.position.x===125){
             this.playerAttack=false;
             var range = this.currPokemon.level*8-this.currPokemon.level;
-            var damage = floor(random()*range+10);
+            var damage = floor(random()*range+15);
+            damage*=this.currPokemon.checkElementTypes(this.enemyP.elementType);
             //damage=100;
             this.enemyP.hp-=damage;
             this.npcAttacks=true;
@@ -456,7 +462,8 @@ fightScene.prototype.npcAttack = function(){
             this.npcAttacks=false;
             var range = this.enemyP.level*8-this.enemyP.level;
             //modified to make player win
-            var damage = floor(random()*range+5);
+            var damage = floor(random()*range+12);
+            damage*=this.enemyP.checkElementTypes(this.currPokemon.elementType);
             //damage=100;
             this.currPokemon.hp-=damage;
             this.turn=true;
--- element.js
+++ element.js
@@ -87,3 +87,86 @@ var earthElement = new elementObj("earth");
 var metalElement = new elementObj("metal");
 var waterElement = new elementObj("water");
 var woodElement = new elementObj("wood");
+
+var drawElement = function(type, size, x, y)
+{
+	push();
+	translate(x, y);
+	scale(size);
+	if(type === "fire")
+	{
+		push();
+		fill(224, 138, 0);
+		ellipse(200, 150, 30, 40);
+		bezier(190, 170, 140, 160, 190, 140, 190, 110);
+		bezier(210, 170, 270, 170, 210, 140, 210, 90);
+		triangle(190, 170, 210, 170, 200, 100);
+		pop();
+	}
+	else if(type === "earth")
+	{
+		push();
+		translate(-12, 0);
+		fill(63, 168, 10);
+		stroke(92, 92, 92);
+		bezier(200, 170, 150, 130, 160, 90, 210, 150);
+		bezier(200, 170, 290, 140, 250, 120, 200, 150);
+		bezier(205, 170, 240, 90, 200, 90, 195, 130);
+		line(195, 130, 202, 170);
+		pop();
+	}
+	else if(type === "metal")
+	{
+		    push();
+		translate(25, 15);
+		scale(0.9);
+		fill(130, 130, 130);
+		stroke(0, 0, 0);
+		quad(180, 160, 160, 145, 230, 100, 250, 115);
+		quad(180, 160, 250, 115, 255, 130, 180, 180);
+		quad(180, 160, 180, 180, 150, 160, 160, 145);
+		pop();
+	}
+	else if(type === "water")
+	{
+		push();
+		translate(0, 15);
+		scale(1);
+		fill(39, 93, 217);
+		stroke(0, 0, 0);
+		bezier(198, 160, 148, 140, 188, 120, 198, 95); 
+		bezier(198, 160, 248, 140, 208, 120, 198, 95); 
+		pop();
+	}
+	else
+	{
+		//type = wood
+		push();
+		translate(0, 0);
+		scale(1);
+		fill(138, 100, 17);
+		push();
+		translate(-27, 42);
+		rotate(Math.PI / -15);
+		rect(160, 135, 70, 20, 100);
+		pop();
+		push();
+		translate(-22, 26);
+		rotate(Math.PI / -15);
+		rect(160, 135, 70, 20, 100);
+		pop();
+		push();
+		translate(-10, 40);
+		rotate(Math.PI / -15);
+		rect(160, 135, 70, 20, 100);
+		pop();
+		fill(181, 158, 111);
+		ellipse(174, 133, 18, 18);                
+		ellipse(187, 147, 18, 18);
+		ellipse(168, 150, 18, 18);
+		stroke(0, 0, 0);
+		pop();
+	}
+	
+	pop();
+};
